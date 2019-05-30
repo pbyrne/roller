be1dda474f642e791ea5e837edc431887a0bf9c4
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2019-05-30 15:36:27 -0500

    Make installable with Homebrew

---
+++ pkg/brew/dice-roller.rb
@@ -0,0 +1,17 @@
+class DiceRollerBin < Formula
+  version "0.1.3"
+  desc "Roll dice from the command line."
+  homepage "https://github.com/pbyrne/roller"
+
+  if OS.mac?
+      url "https://github.com/pbyrne/roller/releases/download/#{version}/dice-roller-#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "92446c6b28b6c726f91ad66a03bcd533fc6e1a28ef4b44c27bfe2d49a0f88531"
+  elsif OS.linux?
+      url "https://github.com/pbyrne/roller/releases/download/#{version}/dice-roller-#{version}-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "ce74cabac9b39b1ad55837ec01e2c670fa7e965772ac2647b209e31ead19008c"
+  end
+
+  def install
+    bin.install "roll"
+  end
+end
