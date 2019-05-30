3d13f77020ed960202e639e2094dec256d0cafcd
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2019-05-30 15:41:40 -0500

    Fix URLs in Homebrew formula

--- pkg/brew/dice-roller.rb
+++ pkg/brew/dice-roller.rb
@@ -4,10 +4,10 @@ class DiceRoller < Formula
   homepage "https://github.com/pbyrne/roller"
 
   if OS.mac?
-      url "https://github.com/pbyrne/roller/releases/download/#{version}/dice-roller-#{version}-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/pbyrne/roller/releases/download/v#{version}/dice-roller-v#{version}-x86_64-apple-darwin.tar.gz"
       sha256 "92446c6b28b6c726f91ad66a03bcd533fc6e1a28ef4b44c27bfe2d49a0f88531"
   elsif OS.linux?
-      url "https://github.com/pbyrne/roller/releases/download/#{version}/dice-roller-#{version}-x86_64-unknown-linux-musl.tar.gz"
+      url "https://github.com/pbyrne/roller/releases/download/v#{version}/dice-roller-v#{version}-x86_64-unknown-linux-musl.tar.gz"
       sha256 "ce74cabac9b39b1ad55837ec01e2c670fa7e965772ac2647b209e31ead19008c"
   end
 
