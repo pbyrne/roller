158abde36b00730856c2b0fe2ca0bfc27929131c
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2019-07-27 19:50:42 -0500

    Updated SHAs for v1.0.0 Homebrew install

--- pkg/brew/dice-roller.rb
+++ pkg/brew/dice-roller.rb
@@ -5,10 +5,10 @@ class DiceRoller < Formula
 
   if OS.mac?
       url "https://github.com/pbyrne/roller/releases/download/v#{version}/dice-roller-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "7af757c4cc9c41023031ebc0d67245b80de46cc6e07ef0d7a0368438c0cd2173"
+      sha256 "ac026d00f6f16765e70b25cd73122157ef72f5364ae552ec0864b8b34896814b"
   elsif OS.linux?
       url "https://github.com/pbyrne/roller/releases/download/v#{version}/dice-roller-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "7df56571413a77ec1ff4998c7d504d086530b5140afd54d51bb86f1df4f9033c"
+      sha256 "3af76865d3877ee67a958e6e4aa517efda93dbeba7b29d55e865fd96d3b47968"
   end
 
   def install
