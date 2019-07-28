382813536ce819a783c557bbb25bfd05872c4e95
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2019-07-27 19:59:46 -0500

    Run through `cargo fmt`

--- src/bin/roll.rs
+++ src/bin/roll.rs
@@ -1,5 +1,5 @@
 extern crate clap;
-use clap::{Arg, App, crate_authors, crate_description, crate_version};
+use clap::{crate_authors, crate_description, crate_version, App, Arg};
 use dice_roller::runner::Runner;
 
 fn main() {
@@ -7,16 +7,23 @@ fn main() {
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .arg(Arg::with_name("verbose")
-             .long("verbose")
-             .takes_value(false)
-             .help("Display additional information"))
-        .arg(Arg::with_name("DEFINITION")
-             .help("Define dice to roll in dice notation (e.x., 2d10, d6+2, 10d8-2)")
-             .default_value("1d6")
-             .index(1))
+        .arg(
+            Arg::with_name("verbose")
+                .long("verbose")
+                .takes_value(false)
+                .help("Display additional information"),
+        )
+        .arg(
+            Arg::with_name("DEFINITION")
+                .help("Define dice to roll in dice notation (e.x., 2d10, d6+2, 10d8-2)")
+                .default_value("1d6")
+                .index(1),
+        )
         .get_matches();
 
-    let runner = Runner::new(config.value_of("DEFINITION").unwrap().to_string(), config.is_present("verbose"));
+    let runner = Runner::new(
+        config.value_of("DEFINITION").unwrap().to_string(),
+        config.is_present("verbose"),
+    );
     runner.run()
 }
